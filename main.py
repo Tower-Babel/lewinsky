b7dcadfbdd012540d691f11232a2eddf870af3a8
Tower-Babel <123087201+Tower-Babel@users.noreply.github.com>
2023-08-31 15:18:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -56,16 +56,6 @@ elif page == "Resources":
     st.header("Resources")
     st.write("This is the Resource page.")
 
-      def displayPDF('paper.pdf'):
-          # Opening file from file path
-          with open(file, "rb") as f:
-              base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-      
-          # Embedding PDF in HTML
-          pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>' 
-      
-          # Displaying File
-          st.markdown(pdf_display, unsafe_allow_html=True) 
     
 
 elif page == "Project":
